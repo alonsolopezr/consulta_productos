2d70f56e80a6a121e8486770429602550cbc1d32
Alonso López Romo <alonsolopez@uthermosillo.edu.mx>
2023-02-17 20:34:33 -0700

    Primer sesion con acweso a BD productos
    Signed-off-by: Alonso López Romo <alonsolopez@uthermosillo.edu.mx>

---
+++ bd/bd_config.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE productos_pdv;
+
+use productos_pdv;
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `codigo_barras` varchar(20) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `precio` double NOT NULL,
+  `imagen` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `codigo_barras`, `descripcion`, `precio`, `imagen`) VALUES
+(1, 'Maruchan Habanero Rebelde', '123123123', 'Maruchan limón camarón habanero', 16.5, '123123');
